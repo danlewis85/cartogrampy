5b546a9e2f06ffa80a670065eba02cf893cc676f
finnkauski <iwiivi@gmail.com>
2019-01-28 19:11:27 +0000

    started adding dorling. issue with plotting it after in main.py

--- cartogrampy/components/cartogram.py
+++ cartogrampy/components/cartogram.py
@@ -1,9 +1,41 @@
+from pysal.weights.Contiguity import Rook, W
 from numpy import mean, pi, power, maximum, minimum, array, concatenate, lexsort, any, argsort, searchsorted, split, take
 import numpy as np
 from numpy.linalg import norm
 from shapely.geometry import Polygon
 from geopandas import GeoDataFrame
 import pandas as pd 
+from shapely.geometry import asShape
+from pysal.weights.Contiguity import Rook, W
+from shapely.geometry import asShape
+from geopandas import GeoDataFrame
+from pandas import DataFrame
+from numpy import sum, pi, power
+from shapely.affinity import translate
+def borders_from_dataframe(df, idVariable=None,geom_field = 'geometry'):
+
+    """Returns a PySAL weights object in which weights are lengths of shared border.
+
+    Args:
+        df (geopandas.geodataframe.GeoDataFrame): Input GeoDataFrame.
+        idVariance (optional): Field name of values uniquely identifying rows in geodataframe, defaults to None.
+        geom_field (str, optional): Field name of geometry column in input GeoDataFrame, defaults to 'geometry'.
+
+    Returns:
+        PySAL W weights object.
+    """
+
+    # Function to compute neighbors and length of shared borders
+    # Based on: https://github.com/volaya/processing_pysal/blob/master/ext-libs/pysal/contrib/shared_perimeter_weights.py
+
+    rook = Rook.from_dataframe(df,idVariable=idVariable)
+    polygons = df.set_index(idVariable)[geom_field].apply(lambda x: asShape(x)).to_dict()
+    new_weights = {}
+    for i in rook.neighbors:
+        a = polygons[i]
+        new_weights[i] = [a.intersection(polygons[j]).length for j in rook.neighbors[i]]
+    return W(rook.neighbors,new_weights)
+
 class Cartogram:
     def __init__(self,
                gdf,
@@ -30,8 +62,142 @@ class Cartogram:
               friction=0.25,
               iterations=99,
               verbose=True):
-        pass
-    
+
+        if self.id_field:
+            if position in self.gdf.columns.values:
+                df = self.gdf[[self.id_field,
+                               self.value_field,
+                               self.geom_field,
+                               position]].copy()
+                id_field = self.id_field
+            else:
+                df = self.gdf[[self.id_field,
+                               self.value_field,
+                               self.geom_field,
+                               position]].copy()
+                id_field = self.id_field
+        else:
+            if position in self.gdf.columns.values:
+                df = self.gdf[[self.value_field,
+                               self.geom_field,
+                               position]].copy()
+                df['id_field'] = df.index
+                id_field = 'id_field'
+            else:
+                df = self.gdf[[self.value_field,
+                               self.geom_field]].copy()
+                df['id_field'] = df.index
+                id_field = 'id_field'
+
+        # get lengths of shared borders
+        wp = borders_from_dataframe(df, id_field, self.geom_field)
+
+        # get dictionary of perimeters
+        perimeter = df.set_index(id_field)[self.geom_field].length.to_dict()
+
+        # now convert df to geodataframe of centers according to pos.
+        if position in df.columns.values:
+            try:
+                df = GeoDataFrame(df, geometry=df[position])
+            except:
+                pass
+        elif position.lower() == 'centroid':
+            df = GeoDataFrame(df, geometry=df[self.geom_field].centroid)
+        elif position.lower() in ['center', 'centre']:
+            df = GeoDataFrame(df, geometry=df[self.geom_field].envelope.centroid)
+        elif position.lower() in ['representative', 'rep']:
+            df = GeoDataFrame(df, geometry=df[self.geom_field].representative_point())
+        else:
+            print("Did not recognize position argument, using centroid")
+            df = GeoDataFrame(df, geometry=df[self.geom_field].centroid)
+
+
+        # work out scale and radii - seems inefficient to deal with the geometries in this way.
+        # no idea how to write this is a PEP8 compliant way.
+        total_dist = sum([df.loc[df[id_field] == i,self.geom_field].centroid.tolist()[0].distance(df.loc[df[id_field] == j,self.geom_field].centroid.tolist()[0]) for i in wp.neighbors for j in wp[i]])
+        total_radius = sum([power(df.loc[df[id_field] == i,self.value_field].values[0]/pi,0.5) + power(df.loc[df[id_field] == j,self.value_field].values[0]/pi,0.5) for i in wp.neighbors for j in wp[i]])
+
+        scale = total_dist / total_radius
+
+        radius = df.set_index(id_field)[self.value_field].apply(lambda x: scale * power(x/pi, 0.5)).to_dict()
+        widest = radius[max(radius, key=lambda i: radius[i])]
+
+        if verbose:
+            print('Scaling by ', scale, ' widest is ', widest)
+
+        # start the main loop
+        for i in range(iterations):
+            displacement = 0.0
+            for idx, row in df.iterrows():
+                # set up for values for each iteration
+                xrepel = 0.0
+                yrepel = 0.0
+                xattract = 0.0
+                yattract = 0.0
+                closest = widest
+
+                # get neighboring circles
+                distband = row[self.geom_field].centroid.buffer(widest + radius[row[id_field]])
+                neighbors = df[df[self.geom_field].centroid.within(distband)]
+                # remove self from neighbors dataframe
+                neighbors = neighbors[neighbors[id_field] != row[id_field]]
+
+                # for neighbors calculate the attractive and repulsive forces acting
+                if len(neighbors) > 0:
+
+                    for nidx, nrow in neighbors.iterrows():
+                        dist = row[self.geom_field].centroid.distance(nrow[self.geom_field].centroid)
+                        if dist < closest:
+                            closest = dist
+                        overlap = radius[row[id_field]] + radius[nrow[id_field]] - dist
+                        # calculate repelling forces for instersecting circles
+                        if overlap > 0.0:
+                            if dist > 1.0:
+                                xrepel -= overlap * (nrow[self.geom_field].centroid.x - row[self.geom_field].centroid.x) / dist
+                                yrepel -= overlap * (nrow[self.geom_field].centroid.y - row[self.geom_field].centroid.y) / dist
+
+                        # calculate attractive forces for circles
+                        if overlap < 0.0:
+                            try:
+                                overlap = abs(overlap) * wp[row[id_field]][nrow[id_field]]/perimeter[row[id_field]]
+                            except KeyError:
+                                overlap = 0.0
+                            xattract = xattract + overlap * (nrow[self.geom_field].centroid.x - row[self.geom_field].centroid.x) / dist
+                            yattract = yattract + overlap * (nrow[self.geom_field].centroid.y - row[self.geom_field].centroid.y) / dist
+
+                # Calculate combined effect of attraction and repulsion
+                atrdst = power(power(xattract, 2) + power(yattract, 2), 0.5)
+                repdst = power(power(xrepel, 2) + power(yrepel, 2), 0.5)
+                if repdst > closest:
+                    xrepel = closest * xrepel / (repdst + 1.0)
+                    yrepel = closest * yrepel / (repdst + 1.0)
+                    repdst = closest
+                if repdst > 0:
+                    xtotal = (1.0 - ratio) * xrepel + ratio * (repdst * xattract / (atrdst + 1.0))
+                    ytotal = (1.0 - ratio) * yrepel + ratio * (repdst * yattract / (atrdst + 1.0))
+                else:
+                    if atrdst > closest:
+                        xattract = closest * xattract / (atrdst + 1.0)
+                        yattract = closest * yattract / (atrdst + 1.0)
+                    xtotal = xattract
+                    ytotal = yattract
+                displacement += power(power(xtotal, 2) + power(ytotal, 2), 0.5)
+
+                # Record the vectors
+                xvector = friction * xtotal
+                yvector = friction * ytotal
+
+                # update position
+                df.loc[idx, self.geom_field] = translate(row[self.geom_field], xoff=xvector, yoff=yvector)
+
+            displacement = displacement/len(df)
+
+            if verbose:
+                print("iter: ",i," displacement: ", displacement)
+
+        df = df.merge(DataFrame(radius.items(), columns=[id_field, 'Radius']),on = id_field)
+        return GeoDataFrame(df,geometry=[df.loc[b,'geometry'].buffer(df.loc[b,'Radius']) for b in range(len(df))])
+
     def dcn(self,
           iterations=99,
           verbose=True):
@@ -155,6 +321,6 @@ class Cartogram:
                 # Dissolve back to original multipart polygon.
                 gdf = gdf.dissolve(by = id_field,as_index=False)
             if verbose:
-                print("iteration:", i+1, "Mean Error:", mean(maximum(gdf[self.geom_field].area,Desired)/minimum(gdf[self.geom_field].area,desired_area)),"Max Error:",max(maximum(gdf[geom_field].area,desired_area)/minimum(gdf[self.geom_field].area,desired_area)))
+                print("iteration:", i+1, "Mean Error:", mean(maximum(gdf[self.geom_field].area,Desired)/minimum(gdf[self.geom_field].area,desired_area)),"Max Error:",max(maximum(gdf[self.geom_field].area,desired_area)/minimum(gdf[self.geom_field].area,desired_area)))
 
         return gdf
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import geopandas as gpd
 import pandas as pd
-# from cartogrampy.components.cartogram import Cartogram
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from cartogrampy.components.cartogram import Cartogram
 
 ldn_boro = gpd.read_file('data/LDN_Boro.geojson').to_crs(epsg=27700)
 ldn_pop  = pd.read_excel('data/Pandas_Lon_Pop.xlsx')
 data = ldn_boro.merge(ldn_pop, how='left', left_on='GSS_CODE', right_on='New Code')
 c = Cartogram(data, value_field=2015 )
-# c.dcn(verbose=True)
+cc = c.dorling(iterations=200,verbose=True)
+# Map the outcome
+fig, ax = plt.subplots(1,2,figsize=(16,8))
+ldn_boro.plot(column = 2015,cmap = 'PuRd', scheme = 'Quantiles', k=4,legend=True, ax=ax[0])
+cc.plot(color='r',ax=ax[1])
+ax[0].axis('equal')
+ax[1].axis('equal')
